4ee426a4b6ea49d1498d0240935b6a637ffd0392
Larysa86 <105893148+Larysa86@users.noreply.github.com>
2022-10-22 13:37:52 +0000

    Deploying to gh-pages from @ Larysa86/goit-js-hw-09@21a7750d9f2d4fc365c8f9c23b5115493cb22e4a 🚀

---
+++ 01-color-switcher.d6b12e02.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n=null;t.startBtn.addEventListener("click",(function(){t.startBtn.setAttribute("disabled",!0),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.d6b12e02.js.map
